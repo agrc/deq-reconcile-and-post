f164733c25773a6b22b3fefc212627dce259eb85
Zach Beck <zbeck@utah.gov>
2015-06-10 10:48:28 -0600

    Ready for testing
    RnP versions created, reconciled/posted, deleted, and re-created.

--- RandP_UIC.py
+++ RandP_UIC.py
@@ -6,10 +6,6 @@ uicAdmin = r'Database Connections\UICAdmin.DefaultSpecific.sde'
 uicBAriotti = r'Database Connections\BAriotti.sde'
 uicCCady = r'Database Connections\CCady.sde'
 
-arcpy.env.workspace = uicAdmin
-
-log = r'C:\\'
-
 default = 'sde.DEFAULT'
 uicSurrogate = 'UICADMIN.UIC_Surrogate_Default'
 uicQA = 'UICADMIN.UIC_QA'
@@ -18,81 +14,92 @@ bAriotti = 'BARIOTTI.UIC_BAriotti'
 cCady_rnp = 'UICADMIN.UIC_RnP_CCady'
 bAriotti_rnp = 'UICADMIN.UIC_RnP_BAriotti'
 
-sourceVersions = [default, uicSurrogate, uicQA, cCady, bAriotti]
-versionLst = []
+arcpy.env.workspace = uicAdmin
 
-for version in arcpy.da.ListVersions(uicAdmin):
-    versionLst.append(version.name)
-    print version.name
-for sVersion in sourceVersions:
-    if sVersion not in versionLst:
-        print 'Insufficient Versions Exist'
-        sys.exit()
-    else:
-        continue
+#---Reconcile and Post---
+def RandP_Versions():
 
-if cCady_rnp not in versionLst:
-    try:
-        arcpy.CreateVersion_management(uicAdmin, cCady, 'UIC_RnP_CCady', 'PRIVATE')
-    except:
-        print 'Could not create ' + cCady_rnp
-        sys.exit()
+    #---Check for necessary versions
+    versionLst = []
+    sourceVersions = [default, uicSurrogate, uicQA, cCady, bAriotti]
+    log = r'C:\\'
 
-if bAriotti_rnp not in versionLst:
-    try:
-        arcpy.CreateVersion_management(uicAdmin, bAriotti, 'UIC_RnP_BAriotti', 'PRIVATE')
-    except:
-        print 'Could not create ' + bAriotti_rnp
-        sys.exit()
+    for version in arcpy.da.ListVersions(uicAdmin):
+        versionLst.append(version.name)
+    for sVersion in sourceVersions:
+        if sVersion not in versionLst:
+            print 'Insufficient Versions Exist'
+            sys.exit()
+        else:
+            continue
 
 
+    #---Create RnP versions if they don't exist
+    if cCady_rnp not in versionLst:
+        try:
+            arcpy.CreateVersion_management(uicAdmin, cCady, 'UIC_RnP_CCady', 'PRIVATE')
+        except:
+            print 'Could not create ' + cCady_rnp
+            sys.exit()
+
+    if bAriotti_rnp not in versionLst:
+        try:
+            arcpy.CreateVersion_management(uicAdmin, bAriotti, 'UIC_RnP_BAriotti', 'PRIVATE')
+        except:
+            print 'Could not create ' + bAriotti_rnp
+            sys.exit()
 
 
-# try:
-#     for version in versionDict:
-#         arcpy.ReconcileVersion_management(uicAdmin, 'BLOCKING_VERSIONS', uicQA, \
-#                                           version, 'LOCK_ACQUIRED', 'ABORT_CONFLICT', 'BY_OBJECT', \
-#                                           '#', '#', '#', '#', log + versionDict[version] + 'ERRORS')
-#
-#         print 'Reconciled ' + versionDict[version]
-#
-# except:
-#     print 'Failed to Reconcile Edit Versions'
-#     sys.exit()
-#
-#
-# try:
-#     arcpy.ReconcileVersion_management(uicAdmin, 'ALL_VERSIONS', uicSurrogate, uicQA,
-#                                       'LOCK_ACQUIRED', 'ABORT_CONFLICT', 'BY_OBJECT', \
-#                                       '#', '#', '#', '#', log + 'UIC_QA_ERRORS')
-#
-#     arcpy.ReconcileVersion_management(uicAdmin, 'ALL_VERSIONS', default, uicSurrogate,
-#                                       'LOCK_ACQUIRED', 'ABORT_CONFLICT', 'BY_OBJECT', \
-#                                       '#', '#', '#', '#', log + 'UIC_Surrogate_ERRORS')
-#
-# except:
-#     print 'Failed to Reconcile QA and Surrogate Versions'
-#     sys.exit()
-#
-#
-#
-# for version in arcpy.da.ListVersions(uicAdmin):
-#
-#     if version.name != default:
-#         arcpy.DeleteVersion_management(uicAdmin, version.name)
-#
-# arcpy.Compress_management(uicAdmin)
-#
-#
-#
-#
-# #  connected as UICAdmin:
-# arcpy.CreateVersion_management(uicAdmin, default, uicSurrogate, "PROTECTED")
-#
-# arcpy.CreateVersion_management(uicAdmin, uicSurrogate, uicQA, "PROTECTED")
-#
-# #  connected as BAriotti:
-# arcpy.CreateVersion_management(uicBAriotti, uicQA, 'BARIOTTI.UIC_BAriotti', "PROTECTED")
-#
-# # connected as CCady:
-# arcpy.CreateVersion_management(uicCCady, uicQA, 'CCADY.UIC_CCady', "PROTECTED")
+    today = str(datetime.date.today())
+
+    #---Reconcile/Post RnP Versions to QA
+    print 'Reconcile and Post ' + cCady_rnp
+    arcpy.ReconcileVersions_management(uicAdmin, 'ALL_VERSIONS', uicQA, cCady_rnp, 'LOCK_ACQUIRED', 'ABORT_CONFLICTS', \
+                                       'BY_OBJECT', 'FAVOR_EDIT_VERSION', 'POST', '#', log + cCady_rnp + '_RnP' + today + '.txt')
+
+    print 'Reconcile and Post ' + bAriotti_rnp
+    arcpy.ReconcileVersions_management(uicAdmin, 'ALL_VERSIONS', uicQA, bAriotti_rnp, 'LOCK_ACQUIRED', 'ABORT_CONFLICTS', \
+                                       'BY_OBJECT', 'FAVOR_EDIT_VERSION', 'POST', '#', log + bAriotti_rnp + '_RnP' + today + '.txt')
+
+    #---Reconcile/Post QA Version to Surrogate
+    print 'Reconcile and Post ' + uicQA
+    arcpy.ReconcileVersions_management(uicAdmin, 'ALL_VERSIONS', uicSurrogate, uicQA, 'LOCK_ACQUIRED', 'ABORT_CONFLICTS', \
+                                       'BY_OBJECT', 'FAVOR_EDIT_VERSION', 'POST', '#', log + uicQA + '_RnP' + today + '.txt')
+
+    #---Reconcile/Post Surrogate to Default
+    print 'Reconcile and Post ' + uicSurrogate
+    arcpy.ReconcileVersions_management(uicAdmin, 'ALL_VERSIONS', default, uicSurrogate, 'LOCK_ACQUIRED', 'ABORT_CONFLICTS', \
+                                       'BY_OBJECT', 'FAVOR_EDIT_VERSION', 'POST', '#', log + uicSurrogate + '_RnP' + today + '.txt')
+
+
+
+
+
+#---Delete old versions---
+def deleteVersions():
+    versionOwner_Dict = {default:uicAdmin, uicSurrogate:uicAdmin, uicQA:uicAdmin, cCady:uicCCady, \
+                     bAriotti:uicBAriotti, cCady_rnp:uicAdmin, bAriotti_rnp:uicAdmin}
+    childList = []
+
+    for version in arcpy.da.ListVersions(uicAdmin):
+        for child in version.children:
+            childList.append(child.name)
+
+    for deleteChild in reversed(childList):
+        arcpy.DeleteVersion_management(versionOwner_Dict[deleteChild], deleteChild)
+        print 'DELETED ' + deleteChild
+
+
+
+def createVersions():
+    arcpy.CreateVersion_management(uicAdmin, default, uicSurrogate.split('.')[1], 'PROTECTED')
+    arcpy.CreateVersion_management(uicAdmin, uicSurrogate, uicQA.split('.')[1], 'PROTECTED')
+    arcpy.CreateVersion_management(uicCCady, uicQA, cCady.split('.')[1], 'PROTECTED')
+    arcpy.CreateVersion_management(uicBAriotti, uicQA, bAriotti.split('.')[1], 'PROTECTED')
+
+
+
+
+RandP_Versions()
+deleteVersions()
+createVersions()
